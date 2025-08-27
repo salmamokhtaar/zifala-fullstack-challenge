dc758a098f8ecb70753ae900ae5c13fc211dbb7b
salmamokhtaar <salmamuqtaarsiman@gmail.com>
2025-08-27 13:44:25 +0300

    Updated

--- __tests__/api.countries.test.ts
+++
@@ -1,8 +0,0 @@
-import { GET as countriesGET } from "@/app/api/countries/route";
-
-test("returns countries list", async () => {
-  const r = await countriesGET();
-  const data = await r.json();
-  expect(Array.isArray(data.countries)).toBe(true);
-  expect(data.countries.length).toBeGreaterThan(0);
-});
